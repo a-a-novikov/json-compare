85793dae487f315696bba962d684a3d9b4bd17c6
Lexutka <lexutka@gmail.com>
2023-01-31 02:12:36 +0700

    add properties_to_ignore support + docstring + refactoring

--- json_compare/comparator.py
+++ json_compare/comparator.py
@@ -7,48 +7,84 @@ from .log_processor import LogProcessor
 
 
 class JSONComparator:
+    """
+    Provides comparison of two JSON-files.
+    """
+
     def __init__(
         self,
         left_file_path: str,
         right_file_path: str,
-        target_key_path: str | None = None,
+        target_property: str | None = None,
+        properties_to_ignore: list[str] | None = None,
     ):
+        """
+        :param left_file_path: path to file in .JSON format;
+        :param right_file_path: path to file in .JSON format;
+        :param target_property: key for JSON-object's property's name to be a key in comparison
+        of similar objects nested in an array. For example, you have:
+        {"cats": [{"id": 4, "name": "Nyan"}, {"id": 2, "name": "Marx"}]}. If you want to set
+        cat's "id" as a target_key, use "DATA.cats.<array>.id". DATA points out to the root
+        of your JSON and <array> indicates object with target_key is nested in an array;
+        :param properties_to_ignore: a list of objects' properties whose mismatch should be
+        ignored in the process of comparing. Checkout target_key's paragraph to see examples
+        of correct setting the param's items;
+        """
         self.diff_log: LogProcessor = LogProcessor()
-        if target_key_path is not None:
-            self.target_key_path: str = target_key_path
+        self.target_property: str = target_property if target_property else ""
+        self.properties_to_ignore: list[str] = (
+            properties_to_ignore if properties_to_ignore else list()
+        )
         with open(left_file_path, "r") as file:
             self.left_data = json.load(file)
         with open(right_file_path, "r") as file:
             self.right_data = json.load(file)
 
     def compare_with_right(self) -> None:
+        """
+        Looks for how the right JSON-file differs from the left one.
+        :return: None;
+        """
         self.diff_log.log.clear()
-        self.__edit_t_key_path_root("DATA", "RIGHT")
+        self.__edit_target_prop_path_root("DATA", "RIGHT")
         self.__compare()
-        self.__edit_t_key_path_root("RIGHT", "DATA")
+        self.__edit_target_prop_path_root("RIGHT", "DATA")
 
     def compare_with_left(self) -> None:
+        """
+        Looks for how the left JSON-file differs from the right one.
+        :return: None;
+        """
         self.diff_log.log.clear()
-        self.__edit_t_key_path_root("DATA", "LEFT")
+        self.__edit_target_prop_path_root("DATA", "LEFT")
         self.__compare(with_actual=False)
-        self.__edit_t_key_path_root("LEFT", "DATA")
+        self.__edit_target_prop_path_root("LEFT", "DATA")
 
     def full_compare(self) -> None:
+        """
+        Looks for differences between files from both perspectives.
+        :return: None;
+        """
         self.diff_log.log.clear()
-        self.__edit_t_key_path_root("DATA", "RIGHT")
+        self.__edit_target_prop_path_root("DATA", "RIGHT")
         self.__compare()
-        self.__edit_t_key_path_root("RIGHT", "LEFT")
+        self.__edit_target_prop_path_root("RIGHT", "LEFT")
         self.__compare(with_actual=False)
-        self.__edit_t_key_path_root("LEFT", "DATA")
+        self.__edit_target_prop_path_root("LEFT", "DATA")
 
     def save_diff_logs(self, path: str = "") -> None:
+        """
+        Saves recorded logs as a text file.
+        :param path: a path to directory where the log with differences will be saved.
+        By default, current work directory will be used;
+        :return: None;
+        """
         log_file_name = f"json_compare_diff_{datetime.date.today()}"
         with open(f"{path}{log_file_name}", "w") as f:
             f.write("\n".join(self.diff_log.log))
 
-    def __edit_t_key_path_root(self, from_: str, to: str) -> None:
-        if hasattr(self, "target_key_path"):
-            self.target_key_path = self.target_key_path.replace(from_, to)
+    def __edit_target_prop_path_root(self, from_: str, to: str) -> None:
+        self.target_property = self.target_property.replace(from_, to)
 
     def __compare(self, with_actual: bool = True) -> None:
         data1, data2, root_log = self.left_data, self.right_data, "RIGHT"
@@ -69,7 +105,7 @@ class JSONComparator:
         for key, val in exp_data.items():
             self.diff_log.setup_path(item_path, key)
             if key not in act_data:
-                self.diff_log.missing_key()
+                self.diff_log.missing_property()
                 continue
             if type(val) is dict:
                 if isinstance(act_data[key], dict):
@@ -84,14 +120,14 @@ class JSONComparator:
                     self.diff_log.incorrect_type(list(), act_data[key])
 
                 continue
-            if val != act_data[key]:
+            if val != act_data[key] and not self.__key_to_ignore():
                 self.diff_log.unequal_values(val, act_data[key])
 
     def _compare_lists(
         self, item_path: str, exp_data: list[Any], act_data: list[Any]
     ) -> None:
         if hasattr(self, "target_key_path"):
-            self.__compare_with_nested_obj_key_respect(item_path, exp_data, act_data)
+            self.__compare_with_nested_obj_prop_respect(item_path, exp_data, act_data)
         else:
             self.__compare_with_items_order_respect(item_path, exp_data, act_data)
 
@@ -122,7 +158,7 @@ class JSONComparator:
             if val != act_data[idx]:
                 self.diff_log.unequal_values(val, act_data[idx])
 
-    def __compare_with_nested_obj_key_respect(
+    def __compare_with_nested_obj_prop_respect(
         self, item_path: str, exp_data: list[Any], act_data: list[Any]
     ) -> None:
         exp_data_len, act_data_len = len(exp_data), len(act_data)
@@ -131,13 +167,13 @@ class JSONComparator:
         for idx, val in enumerate(exp_data):
             self.diff_log.setup_path(item_path, f"<array[{idx}]>")
             if type(val) is dict:
-                target_key, target_key_val = self.__get_targets_key_value(val)
+                target_prop, target_prop_val = self.__get_target_prop_value(val)
                 act_data_similar_idx = self.__get_idx_of_similar_row(
-                    target_key, target_key_val, act_data
+                    target_prop, target_prop_val, act_data
                 )
                 if act_data_similar_idx is None:
                     self.diff_log.missing_array_item(
-                        exp_value=target_key_val, spec_key=target_key
+                        exp_value=target_prop_val, target_prop=target_prop
                     )
                     continue
                 self._compare_dicts(
@@ -161,23 +197,31 @@ class JSONComparator:
         elif exp_len < act_len:
             self.diff_log.exceeding_array_items(exp_len, act_len)
 
-    def __get_targets_key_value(
+    def __get_target_prop_value(
         self, object_: dict[str, Any]
     ) -> tuple[str, int | float | str | bool | dict[str, Any] | list[Any] | None]:
-        target_key = self.target_key_path.removeprefix(
+        target_key = self.target_property.removeprefix(
             re.sub("\[[^()]*\]", "", self.diff_log.curr_path)
         ).replace(".", "")
         exp_key_val = object_[target_key]
         return target_key, exp_key_val
 
+    def __key_to_ignore(self) -> bool:
+        curr_path_without_idx = (
+            re.sub("\[[^()]*\]", "", self.diff_log.curr_path)
+            .replace("RIGHT", "DATA")
+            .replace("LEFT", "DATA")
+        )
+        return True if curr_path_without_idx in self.properties_to_ignore else False
+
     @staticmethod
     def __get_idx_of_similar_row(
-        key: str,
-        key_val: int | float | str | bool | dict[str, Any] | list[Any] | None,
+        prop: str,
+        prop_val: int | float | str | bool | dict[str, Any] | list[Any] | None,
         data: list[dict[str, Any]],
     ) -> int | None:
         for idx, obj in enumerate(data):
-            act_val = obj.get(key)
-            if act_val == key_val:
+            act_val = obj.get(prop)
+            if act_val == prop_val:
                 return idx
         return None
--- json_compare/log_processor.py
+++ json_compare/log_processor.py
@@ -12,28 +12,34 @@ class LogProcessor:
         else:
             self.curr_path = f".{key}"
 
-    def missing_key(self) -> None:
-        msg = self.curr_path + "\nkey is missing"
+    def missing_property(self) -> None:
+        msg = self.curr_path + "\nproperty is missing"
         self.log.append(msg)
 
-    def incorrect_type(self, exp_obj: object, act_obj: object) -> None:
+    def incorrect_type(
+            self,
+            exp_obj: int | float | str | bool | dict[str, Any] | list[Any] | None,
+            act_obj: int | float | str | bool | dict[str, Any] | list[Any] | None,
+    ) -> None:
+        exp_obj_type = self.__convert_to_json_type(exp_obj)
+        act_obj_type = self.__convert_to_json_type(act_obj)
         msg = (
-            self.curr_path + f"\nincorrect type: expected {type(exp_obj)}, "
-            f"got {type(act_obj)} instead"
+            self.curr_path + f"\nincorrect type: expected {exp_obj_type}, "
+            f"got {act_obj_type} instead"
         )
         self.log.append(msg)
 
     def lack_of_array_items(self, exp_len: int, act_len: int) -> None:
         msg = (
             self.curr_path
-            + f"\nLack of items in array: expected {exp_len} items, got only {act_len}"
+            + f"\nlack of items in array: expected {exp_len} items, got only {act_len}"
         )
         self.log.append(msg)
 
     def exceeding_array_items(self, exp_len: int, act_len: int) -> None:
         msg = (
             self.curr_path
-            + f"\nToo much items in array: expected {exp_len} items, got {act_len}"
+            + f"\ntoo much items in array: expected {exp_len} items, got {act_len}"
         )
         self.log.append(msg)
 
@@ -50,14 +56,14 @@ class LogProcessor:
             act_value = f'"{act_value}"'
         msg = (
             self.curr_path
-            + f"\nUnequal values: expected {exp_value}, got {act_value} instead"
+            + f"\nunequal values: expected {exp_value}, got {act_value} instead"
         )
         self.log.append(msg)
 
     def missing_array_item(
         self,
         exp_value: int | float | str | bool | dict[str, Any] | list[Any] | None,
-        spec_key: str | None = None,
+        target_prop: str | None = None,
     ) -> None:
         if isinstance(exp_value, str):
             exp_value = f'"{exp_value}"'
@@ -65,7 +71,26 @@ class LogProcessor:
             exp_value = f"<object>"
         if isinstance(exp_value, list):
             exp_value = f"<array>"
-        if spec_key:
-            exp_value = f'<object> with key "{spec_key}"={exp_value}'
-        msg = self.curr_path + f"\nMissing array item: expected {exp_value}"
+        if target_prop:
+            exp_value = f'<object> with "{target_prop}"={exp_value}'
+        msg = self.curr_path + f"\nmissing array item: expected {exp_value}"
         self.log.append(msg)
+
+    @staticmethod
+    def __convert_to_json_type(
+        item: int | float | str | bool | dict[str, Any] | list[Any] | None,
+    ) -> str:
+        if isinstance(item, int):
+            return "<int>"
+        elif isinstance(item, float):
+            return "<float>"
+        elif isinstance(item, str):
+            return "<str>"
+        elif isinstance(item, bool):
+            return "<bool>"
+        elif isinstance(item, dict):
+            return "<object>"
+        elif isinstance(item, list):
+            return "<array>"
+        else:
+            return "<null>"
